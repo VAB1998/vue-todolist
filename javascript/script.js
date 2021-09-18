1090b64193bba8bf4d2bebecd2bba3cc1c743731
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-18 10:06:33 +0200

    Rev. 1: added new features, functions and other general improvements

--- javascript/script.js
+++ javascript/script.js
@@ -7,32 +7,71 @@
   il testo digitato sarà aggiunto alla lista
 */
 let app = new Vue({
-    el: '#app',
+  el: '#app',
 
-    data: {
-        list : ['Cipolla', 'Latte', 'Pasta', 'Cereali', 'Banane'],
+  data: {
+    list : [
+      {
+        toDo : 'Cipolla',
+        done : false
+      }, 
+      {
+        toDo : 'Latte', 
+        done : false
+      },
+      {
+        toDo : 'Pasta', 
+        done : false
+      },
+      {
+        toDo :  'Cereali',
+        done : false
+      }, 
+      {
+        toDo : 'Banane',
+        done : false
+      }
+    ],
+
+    message : ''
+  },
+
+  methods: {
+
+    /**
+     * Delete an object with a known index from the HTML
+     * @param {*} index  Position of the object
+     */
+    deleteItemList : function(index){
         
-        message : ''
+      this.list.splice(index, 1)
+      //Check
+      console.log(index)
     },
 
-    methods: {
+   /**
+    * Adds a new object to the array with the given input message
+    */
+    addToDo(){  
 
-        /**
-         * Delete an object with a known index from the HTML
-         * @param {*} index  Position of the object
-         */
-        deleteItemList : function(index){
-            this.list.splice(index, 1)
-            //Check
-            console.log(index)
-       },
+      const itemToAdd = {
+        toDo : this.message,
+        done : false
+      }
 
-       addItem(){  
-           this.message== '' ? '' : this.list.push(this.message)
-           this.message = ''
-           //Check
-           console.log('Input Received!!')
-       }
+      this.message== '' ? '' : this.list.push(itemToAdd)
+      this.message = ''
+      //Check
+      console.log('Input Received!!')
+    },
 
+    /**
+     * Channges the property value of an object from true to false and vice versa
+     * @param {object} object 
+     */
+    checkItem : function(object){
+      object.done = !object.done
     }
+
+  }
 }); 
